d63ac2a0648bbc190be0e595a73f999c839a804c
Horolsky <horolsky89@outlook.com>
2021-01-13 15:05:02 +0200

    senetstate tested

--- senet/core/cext/senetstate.c
+++ senet/core/cext/senetstate.c
@@ -1,15 +1,33 @@
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
+//#define PY_SSIZE_T_CLEAN
+//#include <Python.h>
 #include "senetstate.h"
-typedef unsigned long long int statedata;
-typedef struct _SenetState {
-    union {
-        statedata bitvalue; //1 bit for agent, 2-4 for steps, other for board
-        struct {
-            statedata agent:1; // current ply active agent 
-            statedata steps:3; // 1-5
-            statedata board:60;// 30x array, 
-        };
-    };
-} sState; // 64 bit state data
 
+char sState_agent(sState s){
+    return (char) s._agent;
+}
+char sState_steps(sState s){
+    return (char) s._steps;
+}
+void sState_board(sState s, char*board){
+    for (int i = 0; i < 30; i++) board[i] = (s._board >> i * 2) % 4;    
+}
+
+sState sState_setAgent(sState s, char agent){
+    s._agent = agent;
+    return s;
+}
+sState sState_setSteps(sState s, char steps){
+    s._steps = steps;
+    return s;
+}
+sState sState_setBoard(sState s, char* board){
+    s._board = 0;
+    for (int i = 0; i < 30; i++) s._board += ((statedata) board[i] << i * 2);
+}
+
+sState sState_setState(char agent, char steps, char* board){
+    sState s = {0};
+    s._agent = agent;
+    s._steps = steps;
+    for (int i = 0; i < 30; i++) s._board += ((statedata) board[i] << i * 2);
+}
--- senet/core/cext/senetstate.h
+++ senet/core/cext/senetstate.h
@@ -1,10 +1,34 @@
 #ifndef SENET_STATE_H
 #define SENET_STATE_H
 
-typedef struct _SenetState sState;
+typedef unsigned long long int statedata;
+/*
+ * game state data, compressed through bitfields
+ * avoid to change values directly
+ */
+typedef struct _SenetState {
+    union {
+        /*
+         * 1st bit for agent, 2d-4d for steps, other for board
+         * right to left indexation
+         */
+        statedata _bitvalue; 
+        struct {
+            statedata _agent:1; // current ply active agent 
+            statedata _steps:3; // 1-5
+            statedata _board:60;// 30x array, 
+        };
+    };
+} sState; 
 
-int sState_agent(sState*);
-int sState_steps(sState*);
-int sState_board(sState*);
+char sState_agent(sState);
+char sState_steps(sState);
+void sState_board(sState, char* board);
+
+sState sState_setAgent(sState, char agent);
+sState sState_setSteps(sState, char steps);
+sState sState_setBoard(sState, char* board);
+
+sState sState_setState(char agent, char steps, char* board);
 
 #endif
