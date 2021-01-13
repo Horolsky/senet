25ba3be016c37f383b7c83900e738d10fdcd3e1c
Horolsky <horolsky89@outlook.com>
2021-01-13 20:17:51 +0200

    folder rename

---
+++ senet/core/extc/senetrules.h
@@ -0,0 +1,16 @@
+#ifndef SENET_RULES_H
+#define SENET_RULES_H
+
+typedef struct _SenetRules {
+    char addmoves[5];//1-5, for empty positions use 0
+} sRules;
+const sRules DEFAULT_RULES = {.addmoves = {1,4,5,0,0}};
+
+/*
+ * return current sRules*
+ * if rules has not been set,
+ * initialize it from DEFAULT_RULES
+ */
+sRules* sRules_get(void);
+sRules* sRules_set(sRules);
+#endif
