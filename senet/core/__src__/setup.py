eae03820949fd61cc7118a222517fb7ca3251ba0
Horolsky <horolsky89@outlook.com>
2021-01-15 00:29:09 +0200

    setup.py cleanup

--- senet/core/__src__/setup.py
+++ senet/core/__src__/setup.py
@@ -3,14 +3,8 @@ from distutils.core import Extension
 from Cython.Build import cythonize
 
 setup(
-    #name='xtc',
-    #ext_modules=cythonize("xtc.pyx"),
+    name="xtc",
     ext_modules=cythonize(Extension("xtc", ["state/*.pyx"])),
     package_data={"xtc":['state/*.pxd']},
     zip_safe=False,
 )
-#Extension("simulate_fast", ["matrix.c", "random_generator.c", "simulate_fast_c.pyx"])
-
-""" from distutils.core import setup, Extension
-setup(name="extc", version="1.0",
-      ext_modules=[Extension("extc", ["src/_senetmodule.c"])]) """
