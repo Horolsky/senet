6c42596cb2105a4ee09982148e757dc891765ef4
Horolsky <horolsky89@outlook.com>
2021-01-15 20:55:40 +0200

    core.game xply impl

--- senet/core/game.py
+++ senet/core/game.py
@@ -1,5 +1,5 @@
 import random as r
-from .state import state
+from .xply import Ply
 from .agent import agent
 from senet.utils import report
 from senet.settings import SETTINGS
@@ -35,10 +35,7 @@ class game():
         @param agent2: agent
         """
         self.__log = SETTINGS.get("dev/gamelogs")
-        rules = {
-            "addmove": SETTINGS.get("rules/addmove")#TODO check 
-        } 
-        #settings changes not affect the running game
+
 
         #agents duck typing
         if not is_agent(agent1) or not is_agent(agent2):
@@ -48,11 +45,12 @@ class game():
         self.__running = True
         self.__turn = 0
         self.__sticks = game.throw_sticks()
-        self.__state = state(None, first, self.steps, rules)
+        self.__state = Ply()
+        self.__state.steps = self.steps
         if self.__log:
             self._report = report(
                 "game", "json", "logs/games", 
-                f'{{\n"players": "1 - {agent1._name}, 2 - {agent2._name}",\n"rules":{dumps(rules)},\n"game": [\n'
+                f'{{\n"players": "1 - {agent1._name}, 2 - {agent2._name}",\n"game": [\n'
                 )
         self.__onmove()
         self.__run()
@@ -62,17 +60,18 @@ class game():
             self._report.write(self.state.to_json())
             
         while self.__running:
-            if self.__log:
-                self._report.write(',\n' + self.state.to_json())
             self.__running = self.__move()
             self.__onmove()
+            if self.__log:
+                self._report.write(',\n' + self.state.to_json())
             #END GAME CONDITION
             if 5 in self.state.bench:
-                self.__onvictory(self.state.event[0]) #sending agent n to callback
+                self.__onvictory(self.state.event[1]) #sending agent n to callback
                 self.stop()
                 if self.__log:
                     self._report.write("\n]\n}")
                     self._report.close()
+
 
     def __move(self):  #manage_movement
         """
@@ -83,7 +82,8 @@ class game():
         """ 
         cell = self.agent.choose_movement(self.state)
         newsticks = game.throw_sticks()
-        newstate = self.state.increment(cell, game.get_steps(newsticks)) 
+        newstate = self.state.increment(cell)
+        newstate.steps = game.get_steps(newsticks) 
         if newstate is None:
             return False  
         else:
