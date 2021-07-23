cc9c6c59ac37129b19ea7d8e0950452c9ba3d145
Oleksandr Khorolskyi <horolsky89@outlook.com>
2021-07-23 22:30:57 +0300

    temp crutch for xtc installation

--- launch_cli.py
+++ launch_cli.py
@@ -1,3 +1,23 @@
-import senet
+#!/usr/bin/env python3
+
+from os import system
+from os.path import isdir
+
 if __name__ == "__main__":
-    senet.launch("cli")
+    try:
+        from senet import launch
+        launch("cli")
+    except ModuleNotFoundError:
+        confirm = input("core extension module is missing, run installation process [Y/n]?")
+        if confirm in ['Y', 'y']:
+            #system("python3 senet/xtc/setup.py build_ext --inplace")
+            system("""
+            cd ./senet/xtc/
+            python3 setup.py build_ext --inplace
+            cd ../../
+            """)
+            print("extension module installed sucessfully\n\n")
+            from senet import launch
+            launch("cli")
+        else:
+            print("unable to launch the game")
