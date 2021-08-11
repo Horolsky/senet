86bb8b85fe11d52a92c0f1937f0dc654d4c53bf4
Oleksandr Khorolskyi <horolsky89@outlook.com>
2021-08-11 18:27:02 +0300

    breaking: empty cell is not 0, ns restruct, enums

--- senet/xtc2/src/xtc-ply.hpp
+++ senet/xtc2/src/xtc-ply.hpp
@@ -2,97 +2,106 @@
 #include "../lib/bitf/src/bitf_solid.hpp"
 #include "xtc.fwd.hpp"
 
-#include <inttypes.h>
 #include <vector>
+
+namespace senet
+{
 
 /**
  *  1 bit:  agent - 0 or 1
  *  3 bits: steps - 0 to 5
- * 60 bits: board - 2-bit offset cell array, 0: empty, 1: 1-agent, 2: 0-agent
+ * 60 bits: board - 2-bit offset cell array, 0: 1-agent, 1: 2-agent, 2: empty
  */
-class xState
-{
-    const uint64_t _seed;
-
-public:
-    static const uint64_t def_state{0b100110011001100110010000};
-    static uint64_t get_seed(uint8_t agent, uint8_t steps, std::vector<uint8_t> board)
+struct xState
+{                                   //0b100110011001100110010000
+    static const uint64_t def_state{0b010001000100010001000000};
+    static uint64_t get_seed(unit_id agent, uint8_t steps, std::vector<uint8_t> board)
     {
-        if (agent > 1 || steps > 5 || board.size() != 30)
+        if ( agent != unit_id::empty || steps > 5 || board.size() != 30)
         {
             throw std::logic_error("invalid xState parameters");
         }
-        int64_t seed{agent};
+        int64_t seed{static_cast<uint64_t>(agent)};
         seed = bitf::solid::insert_scalar<uint64_t, uint8_t>(steps, 1, 3, seed);
         seed = bitf::solid::insert_vector<uint64_t, uint8_t>(board, 4, 60, seed);
         return seed;
     }
+
+    uint64_t _seed;
     xState() : _seed(def_state){};
     xState(uint64_t seed) : _seed(seed){};
-    xState(uint8_t agent, uint8_t steps, std::vector<uint8_t> board) : _seed(get_seed(agent, steps, board)) {}
+    xState(unit_id agent, uint8_t steps, std::vector<uint8_t> board) : _seed(get_seed(agent, steps, board)) {}
     xState(const xState &other) : _seed(other._seed){};
     xState(xState &&other) : _seed(other._seed){};
-    xState &operator=(const xState &other) = delete;
-    xState &operator=(xState &&other) = delete;
+    xState &operator=(const xState &other) { _seed = other._seed; return *this; };
+    xState &operator=(xState &&other) { _seed = other._seed; return *this; };
     ~xState() = default;
 
-    uint8_t agent() const
+    unit_id agent() const
     {
-        return bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, 0, 1);
+        return static_cast<unit_id>(bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, 0, 1));
     }
     uint8_t steps() const
     {
         return bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, 1, 3);
     }
-    uint8_t operator[](uint8_t index) const
+    unit_id operator[](uint8_t index) const
     {
-        return bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, index + 4, 2);
+        return static_cast<unit_id>(bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, index + 4, 2));
     }
     std::vector<uint8_t> board() const
     {
         return bitf::solid::get_vector<uint64_t, uint8_t>(_seed, 4, 2, 2);
     }
-    double utility() const
-    {
 
+    xState& set_agent(unit_id agn)
+    {
+        _seed = bitf::solid::insert_scalar<uint64_t, uint8_t>(static_cast<uint8_t>(agn), 0, 1, _seed);
+        return *this;
+    }
+    xState& set_steps(uint8_t stp)
+    {
+        _seed = bitf::solid::insert_scalar<uint64_t, uint8_t>(stp, 1, 3, _seed);
+        return *this;
     }
 };
 
+
+
 /**
- *  2 bits: direction - 0: no movement, 1: normal, 2: backward
+ *  2 bits: direction of obligatory movement, 0: skip, 1: move, 2: retreat
  *  3 bits: mobility - number of avaliable movements
  * 25 bits: moves - 5-bit offset movement indici
  */
-class xMoves
+struct xMoves
 {
-    const uint32_t _seed;
-
-public:
     static const uint32_t def_state{0b0};
-    static uint32_t get_seed(uint8_t direction, uint8_t mobility, std::vector<uint8_t> moves)
+    static uint32_t get_seed(event_id direction, uint8_t mobility, std::vector<uint8_t> moves)
     {
-        if (direction > 2 || mobility > 5 || moves.size() > 5)
+        if (direction > event_id::retreat || mobility > 5 || moves.size() > 5)
         {
             throw std::logic_error("invalid xMoves parameters");
         }
-        uint32_t seed{direction};
+        uint32_t seed{static_cast<uint32_t>(direction)};
         seed = bitf::solid::insert_scalar<uint32_t, uint8_t>(mobility, 2, 3, seed);
         seed = bitf::solid::insert_vector<uint32_t, uint8_t>(moves, 5, 27, seed);
         return seed;
     }
+    uint32_t _seed;
     xMoves() : _seed(def_state){};
     xMoves(uint32_t seed) : _seed(seed){};
-    xMoves(uint8_t direction, uint8_t mobility, std::vector<uint8_t> moves)
+    xMoves(event_id direction, uint8_t mobility, std::vector<uint8_t> moves)
         : _seed(get_seed(direction, mobility, moves)){};
     xMoves(const xMoves &other) : _seed(other._seed){};
     xMoves(xMoves &&other) : _seed(other._seed){};
-    xMoves &operator=(const xMoves &other) = delete;
-    xMoves &operator=(xMoves &&other) = delete;
+    xMoves &operator=(const xMoves &other) { _seed = other._seed; return *this; };
+    xMoves &operator=(xMoves &&other) { _seed = other._seed; return *this; };
     ~xMoves() = default;
 
-    uint8_t direction() const
+    uint32_t seed() {return _seed; }
+    event_id direction() const
     {
-        return bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, 0, 2);
+        return static_cast<event_id>(bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, 0, 2));
     }
     uint8_t mobility() const
     {
@@ -112,53 +121,45 @@ public:
     }
 };
 /**
- * 1 bit:  agent
- * 3 bits: event code
- * 5 bits: movement chain start
+ * 1 bit:  active agent
+ * 3 bits: event code (see senet::event)
+ * 5 bits: movement chain start position
  * 5 bits: movement chain destination
- * codes:
- * 0 - drow
- * 1 - reverse
- * 2 - skip
- * 3 - move
- * 4 - attack
- * 5 - escape
  */
-class xEvent
+struct xEvent
 {
-    uint16_t _seed;
-
-public:
     static const uint16_t def_state{0b0};
-    static uint16_t get_seed(uint8_t agent, uint8_t eventcode, uint8_t start, uint8_t dest)
+    static uint16_t get_seed(unit_id agent, event_id event, uint8_t start, uint8_t dest)
     {
-        if (agent > 1 || eventcode > 5)
+        if (agent == unit_id::empty)
         {
             throw std::logic_error("invalid xEvent parameters");
         }
-        uint16_t seed = {agent};
-        seed = bitf::solid::insert_scalar<uint16_t, uint8_t>(eventcode, 1, 3, seed);
+        uint16_t seed = {static_cast<uint16_t>(agent)};
+        seed = bitf::solid::insert_scalar<uint16_t, uint8_t>(static_cast<uint8_t>(event), 1, 3, seed);
         seed = bitf::solid::insert_scalar<uint16_t, uint8_t>(start, 4, 5, seed);
         seed = bitf::solid::insert_scalar<uint16_t, uint8_t>(dest, 9, 5, seed);
         return seed;
     }
+    uint16_t _seed;
     xEvent() : _seed(def_state){};
     xEvent(uint32_t seed) : _seed(seed){};
-    xEvent(uint8_t agent, uint8_t eventcode, uint8_t start, uint8_t dest)
-        : _seed(get_seed(agent, eventcode, start, dest)){};
+    xEvent(unit_id agent, event_id event, uint8_t start, uint8_t dest)
+        : _seed(get_seed(agent, event, start, dest)){};
     xEvent(const xEvent &other) : _seed(other._seed){};
     xEvent(xEvent &&other) : _seed(other._seed){};
-    xEvent &operator=(const xEvent &other) = delete;
-    xEvent &operator=(xEvent &&other) = delete;
+    xEvent &operator=(const xEvent &other) { _seed = other._seed; return *this; };
+    xEvent &operator=(xEvent &&other) { _seed = other._seed; return *this; };
     ~xEvent() = default;
+    uint16_t seed() {return _seed; }
 
-    uint8_t agent() const
+    unit_id agent() const
     {
-        return bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, 0, 1);
+        return static_cast<unit_id>(bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, 0, 1));
     }
-    uint8_t code() const
+    event_id code() const
     {
-        return bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, 1, 3);
+        return static_cast<event_id>(bitf::solid::get_scalar<uint64_t, uint8_t>(_seed, 1, 3));
     }
     uint8_t start() const
     {
@@ -188,7 +189,7 @@ public:
     xPly &operator=(xPly &&other) = delete;
     ~xPly() = default;
 
-    uint8_t operator[](uint8_t index) const
+    unit_id operator[](uint8_t index) const
     {
         return _state[index];
     }
@@ -196,7 +197,7 @@ public:
     {
         return _state.board();
     }
-    uint8_t agent() const
+    unit_id agent() const
     {
         return _state.agent();
     }
@@ -209,7 +210,7 @@ public:
     {
         return _moves.moves();
     }
-    uint8_t direction() const
+    event_id direction() const
     {
         return _moves.direction();
     }
@@ -222,4 +223,14 @@ public:
     {
         return _event;
     }
+
+    void set_steps(uint8_t stp)
+    {
+        _state.set_steps(stp);
+    }
+    void set_agent(unit_id agn)
+    {
+        _state.set_agent(agn);
+    }
 };
+} // namespace xtc
--- senet/xtc2/src/xtc.fwd.hpp
+++ senet/xtc2/src/xtc.fwd.hpp
@@ -1,6 +1,35 @@
-class xState;
-class xMoves;
-class xEvent;
-class xPly;
+#include <cstdint>
+namespace senet
+{
+    using uint8_t = std::uint8_t;
+    using uint16_t = std::uint16_t;
+    using uint32_t = std::uint32_t;
+    using uint64_t = std::uint64_t;
 
-double evaluate_state(xState);
+    //board cell unit
+    enum class unit_id : uint8_t
+    {
+        X,
+        Y,
+        empty
+    };
+    //move event type
+    enum class event_id : uint8_t
+    {
+        skip,
+        move,
+        retreat,
+        attack,
+        escape,
+        drow
+    };
+
+    class xState;
+    class xMoves;
+    class xEvent;
+    class xPly;
+
+    enum class game_rules;
+    double evaluate_state(const xState &, game_rules);
+    xState increment_state(const xState &, game_rules);
+}
