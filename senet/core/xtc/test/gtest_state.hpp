39c7b735281f768fe9082d7a1f68dc74e0fd7898
Oleksandr Khorolskyi <horolsky89@outlook.com>
2021-08-13 17:43:34 +0300

    test: default state

--- senet/core/xtc/test/gtest_state.hpp
+++ senet/core/xtc/test/gtest_state.hpp
@@ -7,3 +7,12 @@ TEST (xState, init)
 {
     EXPECT_NO_THROW(xtc::xState());
 }
+
+TEST (xState, def_seed)
+{
+    auto s = xtc::xState();
+    auto board = s.board();
+    std::vector<uint8_t> sample {0,1,0,1,0,1,0,1,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
+    bool eq = std::equal (board.begin (), board.end (), sample.begin ());
+    EXPECT_EQ(eq, true);
+}
