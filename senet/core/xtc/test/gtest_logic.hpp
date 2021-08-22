10d720a43022e5e2663b06fc4033255a868a84b5
Horolsky <horolsky89@outlook.com>
2021-08-22 20:59:51 +0300

    test: get state help function

--- senet/core/xtc/test/gtest_logic.hpp
+++ senet/core/xtc/test/gtest_logic.hpp
@@ -3,7 +3,14 @@
 #include "../src/xtc.logic.hpp"
 #include "gtest/gtest.h"
 
-
+xtc::State get_start_state(xtc::Unit agent, int steps)
+{
+    auto state = xtc::State();
+    state.set_agent(agent);
+    state.set_steps(steps);
+    return state;
+}
+const int start_board[30] {0,1,0,1,0,1,0,1,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
 
 TEST (FuncStrategies, init)
 {
@@ -16,10 +23,7 @@ TEST (FuncIncrement, init)
 
 TEST (FuncStrategies, contains)
 {
-    xtc::Unit agent = xtc::Unit::Y;
-    int steps = 1;
-    int board[30] {0,1,0,1,0,1,0,1,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
-    auto state = xtc::State(agent, steps, board);
+    auto state = get_start_state(xtc::Unit::Y, 1);
     xtc::Strategies s = xtc::FuncStrategies()(state);
     EXPECT_TRUE(s.contains(1));
     EXPECT_TRUE(s.contains(3));
@@ -30,15 +34,20 @@ TEST (FuncStrategies, contains)
 
 TEST (FuncIncrement, increment_1)
 {
-    xtc::Unit agent = xtc::Unit::X;
-    int steps = 1;
-    int board[30] {0,1,0,1,0,1,0,1,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
-    auto state = xtc::State(agent, steps, board);
-
+    auto state = get_start_state(xtc::Unit::X, 1);
     xtc::State new_state = xtc::FuncIncrement()(state,0);
     int new_board[30] {1,0,0,1,0,1,0,1,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
     for (int i = 0; i < 30; i++)
     {
         EXPECT_EQ(new_state.board(i), static_cast<xtc::Unit>(new_board[i]));
     }
+}
+TEST (FuncIncrement, increment_chance_node)
+{
+    
+    auto test = [](){
+        xtc::FuncIncrement incr {};
+        incr(xtc::State(),0);
+    };
+    EXPECT_THROW(test(), std::logic_error);
 }
