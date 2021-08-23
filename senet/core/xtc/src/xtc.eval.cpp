37d3cd79d7d39f71e38357a5680b9a86096bc93b
Horolsky <horolsky89@outlook.com>
2021-08-23 19:32:08 +0300

    xtc: Eval minor refactoring

--- senet/core/xtc/src/xtc.eval.cpp
+++ senet/core/xtc/src/xtc.eval.cpp
@@ -3,11 +3,10 @@
 namespace xtc
 {
 
-
 double
-Eval::expectation (const State& state)
+Eval::expectation (const State &state)
 {
-  uint64_t seed = state.seed();
+  uint64_t seed = state.seed ();
   uint8_t maxSum = 0;
   uint8_t minSum = 0;
   for (uint8_t i = 4; i < 64; i += 2)
@@ -28,12 +27,54 @@ Eval::expectation (const State& state)
   return res;
 }
 
-double Eval::lin_eval(const State &state, const std::array<double,coef_n> coefs)
+double
+Eval::lin_eval (const State &state, const std::array<double, coef_n> &coefs)
 {
   return .5;
 }
-double Eval::poly_eval(const State &state, const std::array<double,coef_n> coefs)
+double
+Eval::poly_eval (const State &state, const std::array<double, coef_n> &coefs)
 {
   return .5;
 }
-} //namespace xtc
+
+Eval::Eval (Eval::Func func_id, std::array<int, coef_n> coefs)
+{
+  std::array<double, coef_n> norm_coefs;
+  int sum = 0;
+  for (int c : coefs)
+    {
+      sum += c;
+    }
+  for (int i = 0; i < coef_n; i++)
+    {
+      norm_coefs[i] = ((double)coefs[i]) / sum;
+    }
+  _func_id = func_id;
+  _coefs = norm_coefs;
+}
+
+double
+Eval::operator() (const State &state) const
+{
+  switch (_func_id)
+    {
+    case Func::BASIC:
+      return expectation (state);
+      break;
+    case Func::LINEAR:
+      return lin_eval (state, _coefs);
+      break;
+    case Func::POLYNOMIAL:
+      return poly_eval (state, _coefs);
+      break;
+    default:
+      break;
+    }
+}
+double
+Eval::coef (int i) const
+{
+  return _coefs[i];
+}
+} // namespace xtc
